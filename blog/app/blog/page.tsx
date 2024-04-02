2431f4d0aeeda29964cc257a3b2e8d7a44771fce
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-02 23:12:39 +0900

    clean: 不要なインポート削除

--- blog/app/blog/page.tsx
+++ blog/app/blog/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-// import styles from "./page.module.css";
 import { client } from "@/libs/client";
 import Link from "next/link";
 import { Blog } from "@/types/blog";
