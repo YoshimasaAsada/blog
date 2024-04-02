ffe638c1884d1f1a3de783479173ccaaf300422d
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-02 23:13:38 +0900

    add: TOCを上に固定するロジックの追加

--- blog/components/TableOfContents.tsx
+++ blog/components/TableOfContents.tsx
@@ -6,12 +6,24 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import IconButton from "@mui/material/IconButton";
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
-// スクロール位置を監視するカスタムフック
-const useScrollPosition = (ref) => {
-  const [isSticky, setSticky] = useState(false);
+type TocProps = {
+  text: string;
+  id: string;
+};
+
+type TableOfContentsProps = {
+  toc: TocProps[];
+  containerRef: React.RefObject<HTMLDivElement>;
+};
+
+/**
+ * スクロール位置を監視し、ヘッダー位置までTOCが上がったらtrue,そうでなければfalse
+ * @param ref reactのrefオブジェクト
+ * @returns
+ */
+const useScrollPosition = (ref: React.RefObject<HTMLDivElement>) => {
+  const [isSticky, setSticky] = useState(false); // TOCを固定するかしないかを判定する
 
   useEffect(() => {
     const boxTop = ref.current ? ref.current.getBoundingClientRect().top : 0;
@@ -33,10 +45,19 @@ const useScrollPosition = (ref) => {
   return isSticky;
 };
 
-export const TableOfContents = ({ toc }) => {
+export const TableOfContents = ({
+  toc,
+  containerRef,
+}: TableOfContentsProps) => {
   const [isExpanded, setExpanded] = useState<boolean>(true);
-  const boxRef = useRef(null); // Boxコンポーネントへの参照
+  const boxRef = useRef<HTMLDivElement | null>(null); // Boxコンポーネントへの参照
   const isSticky = useScrollPosition(boxRef);
+
+  // 親コンテナの幅を動的に取得し、固定表示時に適用
+  // fixが適用された時にレイアウトが崩れるため
+  const containerWidth = containerRef.current
+    ? containerRef.current.offsetWidth
+    : "100%";
 
   const handleLinkClick = (
     event: React.MouseEvent<HTMLDivElement>,
@@ -62,45 +83,38 @@ export const TableOfContents = ({ toc }) => {
       ref={boxRef}
       sx={{
         position: isSticky ? "fixed" : "static",
-        top: isSticky ? "70px" : "0px", // ヘッダーの高さに合わせて調整
-        maxWidth: 360,
-        width: "100%",
+        top: isSticky ? "70px" : "0px",
+        width: containerWidth, // 親コンテナの幅を適用
         zIndex: 2,
         overflow: "auto",
         height: isSticky ? `calc(100vh - 70px)` : "auto",
-        boxShadow: isSticky ? "0 2px 4px rgba(0,0,0,0.2)" : "none",
       }}>
       <Typography variant="h6" sx={{ m: 2 }}>
         目次
       </Typography>
-      <IconButton onClick={handleDrawerToggle} sx={{ m: 2 }}>
-        {isExpanded ? <ExpandLess /> : <ExpandMore />}
-      </IconButton>
-      {isExpanded && (
-        <List>
-          {toc.map((data: any) => (
-            <ListItem
-              key={data.id}
-              component="div"
-              onClick={(e) => handleLinkClick(e, data.id)}
-              sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  pl: data.tag === "h2" ? 2 : data.tag === "h3" ? 4 : 1,
-                }}>
-                <ListItemText
-                  primary={data.tag === "h1" ? data.text : "-" + data.text}
-                  primaryTypographyProps={{
-                    sx: {
-                      "&:hover": { bgcolor: "gray.500", borderRadius: "4px" },
-                    },
-                  }}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      )}
+      <List>
+        {toc.map((data: any) => (
+          <ListItem
+            key={data.id}
+            component="div"
+            onClick={(e) => handleLinkClick(e, data.id)}
+            sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                pl: data.tag === "h2" ? 2 : data.tag === "h3" ? 4 : 1,
+              }}>
+              <ListItemText
+                primary={data.tag === "h1" ? data.text : "-" + data.text}
+                primaryTypographyProps={{
+                  sx: {
+                    "&:hover": { bgcolor: "gray.500", borderRadius: "4px" },
+                  },
+                }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 };
