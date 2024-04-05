9f5bf6e773793a6aa1b08b9845ae707849c2ca39
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-05 23:21:59 +0900

    add: シンタックスハイライトの追加

--- blog/app/blog/[id]/page.tsx
+++ blog/app/blog/[id]/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { client } from "@/libs/client";
-import parse from "html-react-parser";
 import { Blog } from "@/types/blog";
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Container,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { renderToc } from "../../../libs/render-toc";
 import TableOfContents from "@/components/TableOfContents";
 import { CardTest } from "@/components/CardTest";
+import "highlight.js/styles/atom-one-dark.css";
+import hljs, { HighlightResult } from "highlight.js";
+import { load } from "cheerio";
 
 interface PageProps {
   params: {
@@ -44,12 +38,46 @@ export default function Page({ params }: PageProps) {
 
       const toc = renderToc(data.content);
       setToc(toc);
+      const $ = load(data.content);
+
+      // コードブロックのファイル名が入力されている場合の処理
+      $("div[data-filename]").each((_, elm) => {
+        // data-filename属性の値を持つspanを
+        // <div data-filename="{入力したファイル名}">の最初の子要素として追加
+        $(elm).prepend(`<span>${$(elm).attr("data-filename")}</span>`);
+      });
+
+      // コードブロックのシンタックスハイライトを行う
+      $("pre code").each((_, elm) => {
+        const language = $(elm).attr("class") || "";
+        let result: HighlightResult;
+
+        if (language == "") {
+          // 言語が入力なしの場合、自動判定
+          result = hljs.highlightAuto($(elm).text());
+        } else {
+          // 言語が入力ありの場合、入力された言語で判定
+          result = hljs.highlight($(elm).text(), {
+            language: language.replace("language-", ""),
+          });
+        }
+        $(elm).html(result.value);
+        $(elm).addClass("hljs");
+      });
+
+      // 編集したHTMLを再設定
+      data.content = $.html();
     };
 
     if (params && params.id) {
       fetchData();
     }
   }, [params.id]);
+
+  useEffect(() => {
+    // コンテンツがDOMに挿入された後にハイライトを適用
+    hljs.highlightAll();
+  }, [blog]); // blogの状態が更新された後に実行
 
   return (
     <main>
@@ -58,9 +86,12 @@ export default function Page({ params }: PageProps) {
           {/* メインコンテンツ */}
           <Grid item xs={12} md={9}>
             <h1 className="title">{blog?.title}</h1>
-            <div className="blog">
+            {/* <div className="blog">
               {blog?.content ? parse(blog?.content) : null}
-            </div>
+            </div> */}
+            <div
+              className="blog"
+              dangerouslySetInnerHTML={{ __html: blog?.content }}></div>
           </Grid>
           {/* TOCサイドバー */}
           <Grid item xs={12} md={3}>
--- blog/types/blog.ts
+++ blog/types/blog.ts
@@ -1,5 +1,5 @@
 export interface Blog {
   id: string;
   title: string;
-  content: string;
+  content: any;
 }
