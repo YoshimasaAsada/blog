8d2f4d8ca4d50dcf4de396142ca951b659fbdc41
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-09 20:40:35 +0900

    add: ページネーションの作成

--- blog/app/blog/page.tsx
+++ blog/app/blog/page.tsx
@@ -3,7 +3,16 @@ import { useEffect, useState } from "react";
 import { client } from "@/libs/client";
 import Link from "next/link";
 import { Blog } from "@/types/blog";
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Pagination,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 export default function Page() {
   const [blogs, setBlogs] = useState<Blog[] | null>([]); // 初期値を空のオブジェクトに
@@ -19,27 +28,95 @@ export default function Page() {
     fetchData();
   }, []);
 
+  // ページネーションの実装
+  // 1. 状態の設定
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6; // 1ページあたりのアイテム数
+  // 2. データの分割
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = blogs?.slice(indexOfFirstItem, indexOfLastItem);
+  // 3. ページネーションコントロール
+  const paginate = (_, value) => {
+    setCurrentPage(value);
+  };
+
   console.log(blogs);
 
   return (
-    <main>
-      {blogs?.map((content) => (
-        <Link href={`/blog/${content.id}`} key={content.id}>
-          {content.title}
-          <Image
-            className="rounded-lg"
-            src={
-              content.eyecatch?.url
-              // ? content.eyecatch?.url
-              // : `/images/no_image.jpeg`
-            }
-            alt=""
-            width={1200}
-            height={630}
-            style={{ height: "100%" }}
-          />
-        </Link>
-      ))}
-    </main>
+    <Container>
+      <h1 style={{ paddingBottom: "20px", paddingTop: "20px" }}>ブログ一覧</h1>
+      <Grid container spacing={2}>
+        {currentItems.map((content) => (
+          <Grid item xs={4} sm={4} md={4} lg={4} key={content.id}>
+            <Link href={`/blog/${content.id}`}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  height: 300,
+                  "&:hover": {
+                    backgroundColor: "#f5f5f5", // ホバー時の背景色を変更
+                    "& .MuiCardMedia-root": {
+                      transform: "scale(1.05)",
+                      transition: "transform 0.2s ease-in-out",
+                    },
+                  },
+                }}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={content.eyecatch?.url}
+                  alt={content.title}
+                  sx={{
+                    transition: "transform 0.5s ease-in-out",
+                  }}
+                />
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    height: "60%", // カードの残りの高さを埋める
+                  }}>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {content.title}
+                  </Typography>
+                  <div style={{ marginTop: "auto" }}>
+                    {/* 投稿日を下に押し下げる */}
+                    <Typography variant="body2" color="text.secondary">
+                      投稿日：
+                      {new Date(content.publishedAt).toLocaleDateString(
+                        "ja-JP"
+                      )}
+                    </Typography>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+      <Stack spacing={2}>
+        <Pagination
+          count={Math.ceil(blogs?.length / itemsPerPage)} // 全ページ数
+          page={currentPage}
+          onChange={paginate}
+          sx={{
+            paddingTop: "8px",
+            "& .MuiPaginationItem-root": {
+              color: "white", // 非アクティブなアイテムのテキスト色
+              backgroundColor: "transparent", // 非アクティブなアイテムの背景色
+              "&:hover": {
+                backgroundColor: "gray", // ホバー時の背景色
+              },
+            },
+            "& .MuiPaginationItem-root.Mui-selected": {
+              color: "white", // 選択されたアイテムのテキスト色
+              backgroundColor: "gray", // 選択されたアイテムの背景色
+            },
+          }}
+        />
+      </Stack>
+    </Container>
   );
 }
