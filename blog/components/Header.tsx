fe151e4998eaab6ba75bbcf0354e67326b222d21
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-11 05:37:13 +0900

    fix: 不要な記述の削除、アイコン修正

--- blog/components/Header.tsx
+++ blog/components/Header.tsx
@@ -51,7 +51,11 @@ export function Header() {
     <AppBar position="fixed" sx={{ backgroundColor: "rgba(66, 66, 66, 0.8)" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Avatar
+            alt="Remy Sharp"
+            src="/images/942831A9-9E6A-46F0-B9C0-5992CC219822_1_102_o.jpeg"
+            sx={{ width: 70, height: 50, marginRight: "10px" }}
+          />
           <Typography
             variant="h6"
             noWrap
@@ -132,37 +136,6 @@ export function Header() {
                 </Button>
               </Link>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}>
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">
-                    <Link href={`/setting`}>{setting}</Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
