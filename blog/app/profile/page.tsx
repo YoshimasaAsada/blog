a42062fb9d0ae1866e55ca242c626bf40c5bdb1b
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-23 15:11:25 +0900

    add: プロフ画面修正

--- blog/app/profile/page.tsx
+++ blog/app/profile/page.tsx
@@ -1,30 +1,75 @@
 "use client";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { useEffect } from "react";
+import * as THREE from "three";
+import PeriodicTable from "@/components/PeriodicTable";
+import { Container } from "@mui/material";
+import { Profile } from "@/components/Profile";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import LaptopChromebookIcon from "@mui/icons-material/LaptopChromebook";
 
-export default function Home() {
+const Earth = () => {
+  // return <PeriodicTable />;
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/images/942831A9-9E6A-46F0-B9C0-5992CC219822_1_102_o.jpeg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Container>
+        麻田祥正 1999年生まれ
+        {/* <Profile /> */}
+        <Timeline
+          sx={{
+            padding: 0,
+            [`& .${timelineItemClasses.root}:before`]: {
+              flex: 0,
+              padding: 0,
+            },
+          }}>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot>
+                <LaptopChromebookIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <h2>インフラトップトップ（2021/4~現在）</h2>
+              DMM WEB
+              Cammpというプログラミングスクールでメンターを行なっています。 Ruby
+              on Rails, AWSをメインに教えています。
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot>
+                <LaptopChromebookIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <h2>テモナ株式会社（2023/4~現在）</h2>
+              新卒で入りました。
+              情シス、インフラ構築,保守,運用(AlmaLinux)、バックエンド開発(Nestjs×Typescript)、フロントエンド開発(React×Typescript)をやってます。
+              <h3>情シス</h3>
+              主にアクセス権の付与。AWSのIAM、Githubのリポジトリ権限管理など
+              <h3>インフラ</h3>
+              既存製品の保守、運用と新製品(4種)の開発に伴うインフラの構築(さくらサーバーにOS導入から環境構築、死活監視ツール導入、Nginx周りの設定、バックアップの設定など）を行いました。
+              <h3>フロントエンド</h3>
+              Reactで管理画面の作成
+              <h3>バックエンド</h3>
+              NestJSでAPI開発
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+        <h2>Skils</h2>
+        Rails,React,NestJS,Next.JS,Ruby,Typescript,Python,sh
+        <h2>qualification</h2>
+        AWS Certified Solutions Architect - Associate
+      </Container>
+    </>
   );
-}
+};
+
+export default Earth;
