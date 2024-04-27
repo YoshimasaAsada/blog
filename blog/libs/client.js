6bd6cfffad4cd61813a52753b50b48e75d4d404c
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-27 22:55:30 +0900

    fix: deploy失敗の修正

--- blog/libs/client.js
+++ blog/libs/client.js
@@ -3,7 +3,15 @@
  */
 import { createClient } from "microcms-js-sdk";
 
+if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
 export const client = createClient({
-  serviceDomain: "yasd",
+  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
