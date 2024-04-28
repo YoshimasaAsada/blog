3398936d488dfab41f9384f57d6e719353bad7bf
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-04-28 18:39:39 +0900

    add: sitemap修正

--- blog/app/sitemap.ts
+++ blog/app/sitemap.ts
@@ -1,19 +1,3 @@
-// import { MetadataRoute } from "next";
-
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const baseURL = process.env.BASE_URL || ""; // URLは.envに記載
-//   const lastModified = new Date();
-
-//   const staticPaths = [
-//     {
-//       url: baseURL,
-//       lastModified,
-//     },
-//   ];
-
-//   return [...staticPaths];
-// }
-
 import { client } from "@/libs/client";
 
 export default async function sitemap() {
@@ -23,15 +7,9 @@ export default async function sitemap() {
 
   // 記事一覧ページ
   const blogs = allBlogs.contents.map((blog: any) => ({
-    url: `https://yasdtech.com/blogs/${blog.id}`,
+    url: `https://yasdtech.com/blog/${blog.id}`,
     lastModified: blog.createdAt,
   }));
 
-  // その他のページ
-  const routes = ["", "/search"].map((route) => ({
-    url: `https://yasdtech.com${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-
-  return [...routes, ...blogs];
+  return [...blogs];
 }
