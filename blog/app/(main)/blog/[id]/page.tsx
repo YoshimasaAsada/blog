31ebe5fca6bb919f1925397542e1672c6cecdaae
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-03 01:23:36 +0900

    fix: syntaxエラー調査

--- blog/app/(main)/blog/[id]/page.tsx
+++ blog/app/(main)/blog/[id]/page.tsx
@@ -33,26 +33,26 @@ export default async function Page({ params }: { params: { id: string } }) {
   // ここでHydtrationWarning出てる
   const $ = cheerio.load(blog.content);
   // // コードブロックのファイル名が入力されている場合の処理
-  // $("div[data-filename]").each((_, elm) => {
-  //   $(elm).prepend(`<span>${$(elm).attr("data-filename")}</span>`);
-  // });
+  $("div[data-filename]").each((_, elm) => {
+    $(elm).prepend(`<span>${$(elm).attr("data-filename")}</span>`);
+  });
 
   // コードブロックのシンタックスハイライトを行う
-  $("pre code").each((_, elm) => {
-    const language = $(elm).attr("class") || "";
-    let result: HighlightResult;
-    if (language == "") {
-      // 言語が入力なしの場合、自動判定
-      result = hljs.highlightAuto($(elm).text());
-    } else {
-      // 言語が入力ありの場合、入力された言語で判定
-      result = hljs.highlight($(elm).text(), {
-        language: language.replace("language-", ""),
-      });
-    }
-    $(elm).html(result.value);
-    $(elm).addClass("hljs");
-  });
+  // $("pre code").each((_, elm) => {
+  //   const language = $(elm).attr("class") || "";
+  //   let result: HighlightResult;
+  //   if (language == "") {
+  //     // 言語が入力なしの場合、自動判定
+  //     result = hljs.highlightAuto($(elm).text());
+  //   } else {
+  //     // 言語が入力ありの場合、入力された言語で判定
+  //     result = hljs.highlight($(elm).text(), {
+  //       language: language.replace("language-", ""),
+  //     });
+  //   }
+  //   $(elm).html(result.value);
+  //   $(elm).addClass("hljs");
+  // });
 
   return (
     <>
