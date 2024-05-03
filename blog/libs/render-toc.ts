d421cc2f907b76175d9afbffd154141a499ef8b1
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-04 07:27:55 +0900

    clean: 関数コメント追加

--- blog/libs/render-toc.ts
+++ blog/libs/render-toc.ts
@@ -1,14 +1,17 @@
 import * as cheerio from "cheerio";
 
-// 見出しのテキストとID、そしてその階層レベルを含むオブジェクトの配列を返す
+/**
+ * ブログのTOC
+ * @param body ブログコンテンツの中身のHTML
+ * @returns
+ */
 export const renderToc = (body: string) => {
   const $ = cheerio.load(body);
   const headings = $("h1, h2, h3").toArray();
   const toc = headings.map((heading) => {
-    // heading.name でタグ名（h1, h2, h3）を取得し、その数値部分を階層レベルとして使用
     const level = parseInt(heading.tagName[1], 10);
     const text = $(heading).text();
-    const id = $(heading).attr('id') || '';
+    const id = $(heading).attr("id") || "";
 
     return { text, id, level };
   });
