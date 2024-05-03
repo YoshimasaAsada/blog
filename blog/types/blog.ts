3b7d08d1d46faa495c30c0c74349bf988d1c6e4d
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-04 02:40:21 +0900

    update: 更新日の型追加とコメント追加

--- blog/types/blog.ts
+++ blog/types/blog.ts
@@ -3,10 +3,12 @@ import { Category } from "./category";
 export interface Blog {
   id: string;
   title: string;
+  /** blogのコンテンツHTML */
   content: string;
   eyecatch: {
     url: string;
   };
   category: Category[];
   publishedAt: string;
+  updatedAt: string;
 }
