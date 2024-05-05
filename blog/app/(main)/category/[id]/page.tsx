8fde117d405c227d03ed27e6ad345ef9cff8e340
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-05 15:33:39 +0900

    clean: 細かいレイアウトの調整

--- blog/app/(main)/category/[id]/page.tsx
+++ blog/app/(main)/category/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { BlogCard } from "@/components/BlogCard";
 import { CategoryList } from "@/components/CategoryList";
 import { getAllCategories, getBlogsFilterByCategoryId } from "@/libs/client";
-import { Blog } from "@/types/blog";
 import { Container, Fade, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 
@@ -27,19 +26,19 @@ export default async function Page({ params }: { params: { id: string } }) {
         alignItems="center"
         style={{ paddingTop: "20px", paddingBottom: "20px" }}>
         {/* <BlogSwiper blogs={blogs} /> */}
-        <Grid item xs={6}>
+        <Grid item xs={7}>
           <Typography
             variant="h3"
             component="h3"
             style={{
               textDecoration: "underline",
-              textUnderlineOffset: "8px", // 下線とテキストの間の距離を調整
-              textDecorationThickness: "2px", // 下線の太さを調整
+              textUnderlineOffset: "8px",
+              textDecorationThickness: "2px",
             }}>
             Blogs
           </Typography>
         </Grid>
-        <Grid item xs={6} style={{ textAlign: "right" }}>
+        <Grid item xs={2} style={{ textAlign: "right" }}>
           <Link href="/blog" passHref>
             <div
               className="to-blog-animation"
@@ -54,13 +53,21 @@ export default async function Page({ params }: { params: { id: string } }) {
       <Grid container spacing={2}>
         <Grid item xs={9}>
           <Grid container spacing={2}>
-            {blogs.map((content: any, index: number) => (
-              <Grid item xs={12} sm={6} key={content.id}>
-                <Fade in={true} timeout={(index + 1) * 1000}>
-                  <BlogCard content={content} />
-                </Fade>
+            {blogs.length === 0 ? (
+              <Grid item xs={12}>
+                <Typography variant="h5">
+                  ヒットするコンテンツはありません
+                </Typography>
               </Grid>
-            ))}
+            ) : (
+              blogs.map((content: any, index: number) => (
+                <Grid item xs={12} sm={6} key={content.id}>
+                  <Fade in={true} timeout={(index + 1) * 1000}>
+                    <BlogCard content={content} />
+                  </Fade>
+                </Grid>
+              ))
+            )}
           </Grid>
         </Grid>
         <Grid item xs={3}>
