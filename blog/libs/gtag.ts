ca23abbdf6e3f24874a1014ab8a21c5a4c513367
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-05 12:27:14 +0900

    fix: ハイドレーションエラーの調査

--- blog/libs/gtag.ts
+++ blog/libs/gtag.ts
@@ -1,12 +1,12 @@
 /**
  * GoogleAnalyticsのライブラリ
  */
-export const GA_TAG_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
-export const IS_GATAG = GA_TAG_ID !== "";
+export const existsGaId = GA_MEASUREMENT_ID !== "";
 
 export const pageview = (path: string) => {
-  window.gtag("config", GA_TAG_ID, {
+  window.gtag("config", GA_MEASUREMENT_ID, {
     page_path: path,
   });
 };
