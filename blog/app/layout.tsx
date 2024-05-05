e2c92253bd24d6702d593426fe5c4654185ba4f6
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-05 13:49:35 +0900

    add: gaを@next/third-partiesで導入

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+// import GoogleAnalytics from "@/components/GoogleAnalytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,8 @@ export const metadata: Metadata = {
   description: "yasd tech",
 };
 
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head>{/* <GoogleAnalytics /> */}</head>
+      <head>
+        <GoogleAnalytics gaId={GA_ID} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
