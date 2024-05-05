1d7b055f10e08e7a58528980b8c8133aa84843ea
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-05 17:51:08 +0900

    clean: 関数コメント追加

--- blog/libs/client.ts
+++ blog/libs/client.ts
@@ -12,11 +12,18 @@ if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
   throw new Error("MICROCMS_API_KEY is required");
 }
 
+/**
+ * Microcmsが用意しているfetchの雛形。
+ */
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
 
+/**
+ * 全てのブログを取得
+ * @returns
+ */
 export const getAllBlogs = async () => {
   const data = await client.get<BlogsInContents>({
     endpoint: "blogs",
@@ -24,7 +31,12 @@ export const getAllBlogs = async () => {
   return data.contents;
 };
 
-export const getBlogById = async (params: any) => {
+/**
+ * ブログIDから該当するブログを取得
+ * @param params ブログのID
+ * @returns
+ */
+export const getBlogById = async (params: { id: string }) => {
   const data = await client.getListDetail<Blog>({
     endpoint: "blogs",
     contentId: params.id,
@@ -32,6 +44,10 @@ export const getBlogById = async (params: any) => {
   return data;
 };
 
+/**
+ * 全てのカテゴリーを取得
+ * @returns
+ */
 export const getAllCategories = async () => {
   const data = await client.get<BlogsInContents>({
     endpoint: "categories",
@@ -39,7 +55,12 @@ export const getAllCategories = async () => {
   return data.contents;
 };
 
-export const getBlogsFilterByCategoryId = async (params: any) => {
+/**
+ * 特定のカテゴリーを持つブログを全て取得
+ * @param params 検索したいカテゴリーのID
+ * @returns
+ */
+export const getBlogsFilterByCategoryId = async (params: { id: string }) => {
   const data = await client.get<BlogsInContents>({
     endpoint: "blogs",
     queries: {
