f6c49904b2205476fe686697111729dd897c89b6
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-05-06 16:19:20 +0900

    clean: LoadingコンポーネントのスタイルをグローバルCSSから切り離し

--- blog/app/loading.tsx
+++ blog/app/loading.tsx
@@ -2,10 +2,11 @@
  * ロード中のに出すやつ
  */
 import { LinearProgress } from "@mui/material";
+import styles from "./loading.module.css";
 
 export default function Loading() {
   return (
-    <div className="LoadMain">
+    <div className={styles.LoadMain}>
       <div className="progressBarContainer">
         <h2>Now Loading・・・</h2>
         <LinearProgress />
