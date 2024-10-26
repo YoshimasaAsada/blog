13fbafed33c1f83ba959e84fcd7d1fc46f675de2
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-10-27 01:33:04 +0900

    add: pritter導入

---
+++ blog/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off',
+}
