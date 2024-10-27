7f8f94d4e6261caac1dee555030475c250982b81
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-10-27 12:16:17 +0900

    fix: コンテンツがない場合、フッターの下に変なスペースができるやつの解消

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.scss'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://yasdtech.com"),
-  title: "yasd tech",
-  description: "yasdのテックブログです",
+  metadataBase: new URL('https://yasdtech.com'),
+  title: 'yasd tech',
+  description: 'yasdのテックブログです',
   openGraph: {
-    title: "yasd tech",
-    description: "yasdのテックブログです",
-    url: "https://yasdtech.com/",
-    siteName: "yasd tech",
-    locale: "ja_JP",
-    type: "website",
-    images: ["./opengraph-image.png"],
+    title: 'yasd tech',
+    description: 'yasdのテックブログです',
+    url: 'https://yasdtech.com/',
+    siteName: 'yasd tech',
+    locale: 'ja_JP',
+    type: 'website',
+    images: ['./opengraph-image.png'],
   },
   twitter: {
-    card: "summary_large_image",
-    title: "yasd tech",
-    description: "yasdのテックブログです",
-    images: ["./opengraph-image.png"],
+    card: 'summary_large_image',
+    title: 'yasd tech',
+    description: 'yasdのテックブログです',
+    images: ['./opengraph-image.png'],
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID || ''
 
   return (
     <html lang="ja">
@@ -40,5 +40,5 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
