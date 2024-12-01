c5f0b5f1a8bc4c0fbefe55a59268c82f1788ac29
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-12-01 13:48:14 +0900

    fix 使用していないインポートの削除

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
